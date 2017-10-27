bb0343b5e7fc3c132df92e14738ee91da89cf460
LeisanKokorina <leisanz94@mail.ru>
2017-10-27 22:31:32 +0300

    modified

--- Mum/src/ru/itpark/Main.java
+++ Mum/src/ru/itpark/Main.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // макс 150 ответов
         // отлично, все устраивает - 10
         // в целом, все хорошо - 7,5
         // удовл - 5
         // плохо - 2
         Scanner scanner = new Scanner(System.in);
-        double count5 = 0.0;
-        double count4 = 0.0;
-        double count3 = 0.0;
-        double count2 = 0.0;
+        double sum5 = 0.0;
+        double sum4 = 0.0;
+        double sum3 = 0.0;
+        double sum2 = 0.0;
+        double sum1 = 0.0;
+        int yes = 0;
+        int not = 0;
         System.out.println("5. Отл = 10");
         System.out.println("4. Хорошо = 7,5");
         System.out.println("3. Удовл = 5");
-        System.out.println("2. Плохо = 2");
-        System.out.println("1. Вывести счетчик");
+        System.out.println("2. Плохо = 2,5");
+        System.out.println("1. Неуд = 0");
+        System.out.println("6. ДА = 2");
+        System.out.println("7. НЕТ = 0");
+        System.out.println("8. Вывести суммы");
         System.out.println("9. Выход из программы");
 
         while(true){
@@ -28,33 +33,45 @@ public class Main {
             int mark = scanner.nextInt();
             switch (mark){
                 case 5:{
-                     count5 = count5 + 10;
+                     sum5 = sum5 + 10;
 
                 }break;
 
                 case 4:{
-                    count4 = count4 + 7.5;
+                    sum4 = sum4 + 7.5;
                 }break;
                 case 3: {
-                    count3 = count3 + 5;
+                    sum3 = sum3 + 5;
                 }break;
 
                 case 2:{
-                    count2 = count2 + 2;
+                    sum2 = sum2 + 2.5;
                 }break;
                 case 1:{
-                    System.out.println("Отлично = "+count5);
-                    System.out.println("Хорошо = "+count4);
-                    System.out.println("Удовл = "+count3);
-                    System.out.println("Плохо = "+count2);
+                    sum1 = 0;
+
+                }break;
+                case 6:{
+                    yes = yes + 2;
+                }break;
+                case 7: {
+                    not =  0;
+                }break;
+                case 8:{
+                    System.out.println("Отлично = "+sum5);
+                    System.out.println("Хорошо = "+sum4);
+                    System.out.println("Удовл = "+sum3);
+                    System.out.println("Плохо = "+sum2);
+                    System.out.println("Неуд = "+sum1);
+                    double summa = sum1+sum2+sum3+sum4+sum5;
+                    int yesnot = yes + not;
+                    System.out.println("Общая сумма по пункту анкеты = "+summa);
+                    System.out.println("Да/Нет = "+ yesnot);
                 }break;
                 case 9 :{
                     System.exit(0);
                 }break;
-
-
             }
         }
-
     }
 }
